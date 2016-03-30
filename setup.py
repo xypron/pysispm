9ff394c3e6b9e99e4d2cbfe26c09477b93024f95
Heinrich Schuchardt <xypron.glpk@gmx.de>
2016-03-30 11:42:13 +0200

    Bump library version number to 0.2.
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ import sispm
 
 setup(
 	name='pysispm',
-	version='0.1',
+	version='0.2',
 	description='Library for EnerGenie USB controlled powerstrips',
 	author='Heinrich Schuchardt',
 	author_email='xypron.glpk@gmx.de',
